07ed8a1b6584ace47bc444ddf5a384970b2d9b90
Fatih TAN <fatihtann@msn.com>
2022-05-09 16:49:36 +0300

    Project is loaded

---
+++ src/Plugins/Nop.Plugin.Marketplace/Marketplace.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Nop.Services.Cms;
+using Nop.Services.Plugins;
+
+namespace Nop.Plugin.Marketplace
+{
+    public class Marketplace : BasePlugin
+    {
+        public override Task InstallAsync()
+        {
+            return base.InstallAsync();
+        }
+
+        public override Task UninstallAsync()
+        {
+            return base.UninstallAsync();
+        }
+    }
+}
